275cfab4ff9db38bbc365cf9229c61cd3b2bcbb7
Viorel S <vio@agorarobotics.com>
2022-07-28 14:58:22 +0300

    3 buttons on pins 11, 13, 15

--- push_button.py
+++ push_button.py
@@ -1,13 +1,28 @@
+# Board numbering https://pinout.xyz/#
+BUTTON1_PIN = 11
+BUTTON2_PIN = 13
+BUTTON3_PIN = 15
+
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
-def button_callback(channel):
-    print("Button was pushed!")
+def button1_callback(channel):
+    print("Button1 was pushed!")
+
+def button2_callback(channel):
+    print("Button2 was pushed!")
+
+def button3_callback(channel):
+    print("Button3 was pushed!")
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(BUTTON1_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(BUTTON2_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(BUTTON3_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback, bouncetime=500) # Setup event on pin 10 rising edge
+GPIO.add_event_detect(BUTTON1_PIN,GPIO.RISING,callback=button1_callback, bouncetime=500)
+GPIO.add_event_detect(BUTTON2_PIN,GPIO.RISING,callback=button2_callback, bouncetime=500)
+GPIO.add_event_detect(BUTTON3_PIN,GPIO.RISING,callback=button3_callback, bouncetime=500)
 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 
